fe7ff3d464ac9b4b6f0ff905786c6d2149295738
code <ayyoob@codeandhue.com>
2020-08-01 17:07:14 +0530

    aug 1 update

--- src/js/custom.js
+++ src/js/custom.js
@@ -4,4 +4,19 @@ $(document).ready(function () {
         $(this).toggleClass('btn-active');
         $(this).parent().toggleClass('menu-active');
     });
+
+    $('.banner-slider').slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
+    $('.home-image-slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear'
+    });
 });
